7c3bae8829fa91d902f1f9f2c9be932ee503e958
Santos d'Atocha <65743906+mk5749@users.noreply.github.com>
2020-05-21 21:31:16 -0400

    nhp
    first

---
+++ nhp.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fyne.io/fyne"
+    "fyne.io/fyne/app"
+    "fyne.io/fyne/widget"
+)
+
+func main() {
+    a := app.New()
+    win := a.NewWindow("nhp v0")
+	win.Resize(fyne.NewSize(800,600))
+    win.SetContent(widget.NewVBox(
+        widget.NewLabel("Update hero names and picks be here!"),
+        widget.NewButton("Update!", func() {
+            a.Quit()
+        }),
+    ))
+    win.ShowAndRun()
+}
+
+		
+	
