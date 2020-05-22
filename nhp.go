f0f3503afd282d9cd2807cbc8c48355a81126a4f
Santos d'Atocha <65743906+mk5749@users.noreply.github.com>
2020-05-21 21:32:25 -0400

    Update nhp.go

--- nhp.go
+++ nhp.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fyne.io/fyne"
-    "fyne.io/fyne/app"
-    "fyne.io/fyne/widget"
+ 	"fyne.io/fyne/app"
+        "fyne.io/fyne/widget"
 )
 
 func main() {
     a := app.New()
     win := a.NewWindow("nhp v0")
 	win.Resize(fyne.NewSize(800,600))
-    win.SetContent(widget.NewVBox(
-        widget.NewLabel("Update hero names and picks be here!"),
-        widget.NewButton("Update!", func() {
+   	win.SetContent(widget.NewVBox(
+      	 widget.NewLabel("Update hero names and picks be here!"),
+       	 widget.NewButton("Update!", func() {
             a.Quit()
-        }),
+         }),
     ))
     win.ShowAndRun()
 }
